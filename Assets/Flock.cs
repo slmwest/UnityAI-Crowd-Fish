d251019581d70115e295847d386c166417d9ab84
Sheldon <36916808+slmwest@users.noreply.github.com>
2024-03-23 15:11:41 +0000

    add random starting speed per fish

--- Assets/Flock.cs
+++ Assets/Flock.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
--- Assets/FlockManager.cs
+++ Assets/FlockManager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-
+    public static FlockManager FM; // Singleton instance of FlockManager
     public GameObject fishPrefab;
     public int numFish = 20;
     public GameObject[] allFish;
     public Vector3 swimLimits = new Vector3(5, 5, 5);
+
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +30,9 @@ public class FlockManager : MonoBehaviour
             allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
 
         }
+
+        // Set the FlockManager instance to this object
+        FM = this;
     }
 
     // Update is called once per frame
